920e70ae867eedbb73b657c7208e239526bb8e79
Jonathan Nakache <john.nakache@gmail.com>
2020-02-17 18:20:48 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Title from './components/Title'
+import Form from './components/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+        items: [
+          {name: "Acheter le pain"},
+          {name: "Appeler Gilles"}
+        ]
+    }
+  }
+  
+  addItem = (todo) => {
+    this.setState(prevState => ({
+      items :  [...prevState.items, todo]
+    }))
+  } 
+
+  renderItems = () => {
+    return (
+      <ul style={{textAlign:'center', listStyle: 'none'}}>
+        {this.state.items.map(item => 
+          <li key={item.name}>{item.name}</li>
+        )}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+          <Title />
+          <Form addItem={this.addItem}/>
+          {this.renderItems()}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
